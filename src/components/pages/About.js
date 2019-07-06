e6f80eab4fdaac806fcb938fe820446130c594d8
Luke Firth <lukeantonfirth@gmail.com>
2019-07-06 10:53:58 +1000

    profile

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This</h1>
+      <h1>About This</h1>
+    </div>
+  );
+};
+
+export default About;
